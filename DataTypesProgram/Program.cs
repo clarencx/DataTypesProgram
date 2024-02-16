9bbe6dca332232c8be9729be6034c17e148d7078
espina.293013 <espina.293013@LAB-B-PC08>
2024-02-16 09:37:51 +0800

    Add project files.

---
+++ DataTypesProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataType
+{
+    class DataTypeProgram
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the pieces of apple:");
+            int piecesOfApple = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter total price of " + piecesOfApple + " apple(s): ");
+            double priceOfApple = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The total price of " + piecesOfApple + "pieces(s) is " + priceOfApple);
+
+            Console.WriteLine("The value of original price is " + priceOfApple);
+
+            Console.WriteLine("The value of converted price is " + (int)priceOfApple);
+
+            Console.WriteLine("Press any key to exit.... ");
+            Console.ReadKey();
+        }
+    }
+}
+
